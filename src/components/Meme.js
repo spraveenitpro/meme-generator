0f78f6e0916d167fd6bce10afb198442d176a025
Praveen <spraveenitpro@gmail.com>
2022-08-09 16:40:32 -0700

    first commit

---
+++ src/components/Meme.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Meme() {
+    return (
+        <main className="meme">
+            <form className="form">
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Top Text"
+                />
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Bottom Text"
+                />
+                <button className="form--button">Get a new meme Image!!</button>
+            </form>
+        </main>
+    )
+}
