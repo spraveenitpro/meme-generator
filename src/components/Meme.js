8d746c2ba98018ced0f1912040cb12f4082c8f74
Praveen <spraveenitpro@gmail.com>
2022-08-09 22:26:51 -0700

    Making API calls

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import memesData from '../memesData'
 
 export default function Meme() {
+    const [url, setURL] = useState('https://i.imgflip.com/1bgw.jpg')
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        console.log(randomNumber)
+        const { url } = memesArray[randomNumber]
+        setURL(url)
+    }
+
     return (
         <main className="meme">
-            <form className="form">
+            <div className="form">
                 <input
                     type="text"
                     className="form--input"
@@ -14,8 +25,11 @@ export default function Meme() {
                     className="form--input"
                     placeholder="Bottom Text"
                 />
-                <button className="form--button">Get a new meme Image!!</button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>
+                    Get a new meme Image!!
+                </button>
+            </div>
+            <img src={url} />
         </main>
     )
 }
